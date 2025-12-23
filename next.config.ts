ebeacb4c170f60743797b0e66dc61ca222ba4a4e
taqihaider110 <taqihaider591@gmail.com>
2025-12-23 12:22:45 +0500

    turbo removal

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  experimental: {
-    turbo: {
-      enabled: false,
-    },
-  },
+  // leave empty for now
 };
 
 export default nextConfig;
